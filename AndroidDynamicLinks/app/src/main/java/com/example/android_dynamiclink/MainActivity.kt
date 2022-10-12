6db5b03f99f31498bcc10a0ffd427ff1a60edf3d
Jjol_twi <hgymin3129@gmail.com>
2022-10-12 14:29:45 +0900

    main-set links

--- AndroidDynamicLinks/app/src/main/java/com/example/android_dynamiclink/MainActivity.kt
+++ AndroidDynamicLinks/app/src/main/java/com/example/android_dynamiclink/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.android_dynamiclink
 
+import android.content.ContentValues.TAG
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.android_dynamiclink.databinding.ActivityMainBinding
+import com.google.firebase.dynamiclinks.PendingDynamicLinkData
+import com.google.firebase.dynamiclinks.ktx.*
+import com.google.firebase.ktx.Firebase
 
 private lateinit var mbinding: ActivityMainBinding
 
@@ -11,5 +18,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         mbinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mbinding.root)
+    }
+    private fun handleFirebaseDynamiclinks(intent: Intent){
+        Firebase.dynamicLinks
+            .getDynamicLink(intent)
+            .addOnSuccessListener(this) { pendingDynamicLinkData: PendingDynamicLinkData? ->
+                // Get deep link from result (may be null if no link is found)
+                var deepLink: Uri? = null
+                if (pendingDynamicLinkData != null) {
+                    deepLink = pendingDynamicLinkData.link
+                }
+
+                // Handle the deep link. For example, open the linked
+                // content, or apply promotional credit to the user's
+                // account.
+                // ...
+
+            }
+            .addOnFailureListener(this) { e -> Log.w(TAG, "getDynamicLink:onFailure", e) }
     }
 }
